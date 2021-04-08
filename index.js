68f9396dfb1a8352138d31fa13f8750af0cfeee3
sponberry <74988392+sponberry@users.noreply.github.com>
2021-04-08 14:05:31 +0100

    completed exercise 3.5

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
-
 const app = express()
+
+app.use(express.json())
 
 let persons = [
   {
@@ -48,8 +49,23 @@ app.get("/info", (request, response) => {
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(p => p.id !== id)
-
   response.status(204).end()
+})
+
+app.post("api/persons", (request, response) => {
+  const body = request.body
+
+
+  
+  const person = {
+    "name": body.name,
+    "number": body.content,
+    "id": Math.round(Math.random()*1000)
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
