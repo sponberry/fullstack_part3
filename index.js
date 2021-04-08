9b406c9acdbaca123345ec2c0b14a822a87cd71c
sponberry <74988392+sponberry@users.noreply.github.com>
2021-04-08 15:48:26 +0100

    completed exercise 3.8

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express")
+const morgan = require("morgan")
 const app = express()
 
 app.use(express.json())
+
+morgan.token("content", function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 let persons = [
   {
@@ -78,6 +82,12 @@ app.post("/api/persons", (request, response) => {
 
   response.json(person)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error:"unrecognised url endpoint" })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
