d04f93f6614e07902dd15137b7bcdc548bfa3882
sponberry <74988392+sponberry@users.noreply.github.com>
2021-04-08 13:43:34 +0100

    Completed exercise 3.2

--- index.js
+++ index.js
@@ -29,6 +29,12 @@ app.get("/api/persons", (request, response) => {
   response.json(persons)
 })
 
+app.get("/info", (request, response) => {
+  const requestDate = new Date()
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${requestDate.toString()}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`)
