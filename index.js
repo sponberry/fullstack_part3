4f5a7d70e1193d54cb9f2259861c6d0597fe7a9a
sponberry <74988392+sponberry@users.noreply.github.com>
2021-04-11 09:26:13 +0100

    completed exercises 3.13 and 3.14

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config()
 const express = require("express")
 const morgan = require("morgan")
 const app = express()
+const Person = require("./models/person")
 
 app.use(express.static("build"))
 app.use(express.json())
@@ -8,49 +10,56 @@ app.use(express.json())
 morgan.token("content", function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
-let persons = [
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  },
-  {
-    "name": "Wonderwoman",
-    "number": "38492331",
-    "id": 8
-  }
-]
+// let persons = [
+//   {
+//     "name": "Ada Lovelace",
+//     "number": "39-44-5323523",
+//     "id": 2
+//   },
+//   {
+//     "name": "Dan Abramov",
+//     "number": "12-43-234345",
+//     "id": 3
+//   },
+//   {
+//     "name": "Mary Poppendieck",
+//     "number": "39-23-6423122",
+//     "id": 4
+//   },
+//   {
+//     "name": "Wonderwoman",
+//     "number": "38492331",
+//     "id": 8
+//   }
+// ]
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    const people = persons
+    response.json(persons)
+  })
 })
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(p => p.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person.findById(request.params.id).then(persons => {
+    response.json(persons)
+  })
+  // const id = Number(request.params.id)
+  // const person = persons.find(p => p.id === id)
+  // if (person) {
+  //   response.json(person)
+  // } else {
+  //   response.status(404).end()
+  // }
 })
 
 app.get("/info", (request, response) => {
   const requestDate = new Date()
-  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+  response.send(`<p>Phonebook has info for ${people.length} people</p>
     <p>${requestDate.toString()}</p>`)
 })
 
+// ! This will need to be modified for MongoDB later
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(p => p.id !== id)
@@ -65,23 +74,22 @@ app.post("/api/persons", (request, response) => {
       error: "required content missing"
     })
   }
-  persons.map(p => {
-    if (p.name.toLowerCase() === body.name.toLowerCase()){
-      return response.status(400).json({
-        error: "contact with that name already exists"
-      })
-    }
+  // people.map(p => {
+  //   if (p.name.toLowerCase() === body.name.toLowerCase()){
+  //     return response.status(400).json({
+  //       error: "contact with that name already exists"
+  //     })
+  //   }
+  // })
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
   })
-
-  const person = {
-    "name": body.name,
-    "number": body.number,
-    "id": Math.round(Math.random()*1000)
-  }
-
-  persons = persons.concat(person)
-
-  response.json(person)
+  
+  person.save().then(savedContact => {
+    response.json(savedContact)
+  })
 })
 
 const unknownEndpoint = (request, response) => {
@@ -90,7 +98,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`)
 })
